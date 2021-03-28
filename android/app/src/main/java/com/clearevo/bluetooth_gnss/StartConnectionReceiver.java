b319d0b3b5b10c4a4831f10e896989fcefda76d1
Auric Goldfinger <goldfinger.auric@gmail.com>
2021-03-28 11:31:28 +0200

    Move specific code out of Util
    The `overrideConnectionWithOption` is very specific for the `StartConnectionReceiver`, so it doesn't belong in `Util`

--- android/app/src/main/java/com/clearevo/bluetooth_gnss/StartConnectionReceiver.java
+++ android/app/src/main/java/com/clearevo/bluetooth_gnss/StartConnectionReceiver.java
@@ -5,6 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+import com.clearevo.libbluetooth_gnss_service.bluetooth_gnss_service;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -24,10 +31,34 @@ public class StartConnectionReceiver extends BroadcastReceiver {
             if (extras != null) {
                 final String configStr = extras.getString("config");
 
-                Util.overrideConnectionWithOptions(gnssConnectionParams, configStr);
+                overrideConnectionWithOptions(gnssConnectionParams, configStr);
             }
 
             Util.connect(StartConnectionReceiver.class.getName(), context, gnssConnectionParams);
+        }
+    }
+
+    private void overrideConnectionWithOptions(GnssConnectionParams gnssConnectionParams, String overriddenOptions) {
+        Objects.requireNonNull(gnssConnectionParams, "GnssConnection must already been initialised");
+        if (overriddenOptions != null && !overriddenOptions.isEmpty()) {
+            try {
+                final JSONObject overrides = new JSONObject(overriddenOptions);
+                gnssConnectionParams.setBdaddr(overrides.optString("bdaddr", gnssConnectionParams.getBdaddr()));
+                gnssConnectionParams.setSecure(overrides.optBoolean("secure", gnssConnectionParams.isSecure()));
+                gnssConnectionParams.setReconnect(overrides.optBoolean("reconnect", gnssConnectionParams.isReconnect()));
+                gnssConnectionParams.setLogBtRx(overrides.optBoolean("log_bt_rx", gnssConnectionParams.isLogBtRx()));
+                gnssConnectionParams.setDisableNtrip(overrides.optBoolean("disable_ntrip", gnssConnectionParams.isDisableNtrip()));
+
+                final JSONObject overrides_extra_params = overrides.optJSONObject("extra");
+                if (overrides_extra_params != null) {
+                    for (String pk : bluetooth_gnss_service.REQUIRED_INTENT_EXTRA_PARAM_KEYS) {
+                        final String value = overrides_extra_params.optString(pk, gnssConnectionParams.getExtraParams().get(pk));
+                        if (value != null) gnssConnectionParams.getExtraParams().put(pk, value);
+                    }
+                }
+            } catch (JSONException e) {
+                Log.e(Util.class.getSimpleName(), e.getMessage(), e);
+            }
         }
     }
 }
