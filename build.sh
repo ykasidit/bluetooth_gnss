e31c76a4f7a28b3ee0c7dc52da02cd3e56c3c692
Kasidit Yusuf <ykasidit@gmail.com>
2023-12-11 15:55:57 +0700

    upgrade key

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-flutter build apk && ls -l build/app/outputs/flutter-apk/app-release.apk  && cp  build/app/outputs/flutter-apk/app-release.apk app-release.apk && ls -l app-release.apk
+flutter clean && flutter build apk && ls -l build/app/outputs/flutter-apk/app-release.apk  && cp  build/app/outputs/flutter-apk/app-release.apk app-release.apk && ls -l app-release.apk
