92d992446a7c5dc07b0ac2dceb6027939571e06f
Kasidit Yusuf <ykasidit@gmail.com>
2025-01-20 06:01:55 +0700

    adj qstarz ble test cases

--- rust/src/qstarz.rs
+++ rust/src/qstarz.rs
@@ -11,7 +11,7 @@ use kampu::forest::{is_tree_planted, parse_tree, plant_tree};
 use kampu::utils::hex_to_bin;
 
 pub fn parse_qstarz_pkt(payload: Vec<u8>) -> String {
-    init_qstarz_trees();
+    setup_parse_trees();
     if let Ok(ret) = parse_tree(QSTARZ_BLE_SCHEMA_TREE_ID, &payload) {
         return serde_json::to_string(&ret).unwrap();
     }
@@ -45,8 +45,8 @@ dLat = tmp_lat + (dLat - tmp_lat * 100) / 60.0;
 dLon = tmp_lon + (dLon - tmp_lon * 100) / 60.0;
 */
 const QSTARZ_LAT_LON_DDDMM_MMMM_FORMULA_EVAL_STR:&str = r#"
-    tmp_lat = value_float / 100.0;
-    tmp_lat + (value_float - tmp_lat * 100.0) / 60.0
+    tmp_lat = floor(value_float / 100.0);
+    tmp_lat + (value_float - (tmp_lat*100)) / 60.0
     "#;
 const QSTARZ_G_SENSOR_EVAL_STR:&str = "value_int / 256.0";
 const QSTARZ_BLE_SCHEMA_TREE_ID:u64 = 1;
@@ -106,7 +106,7 @@ const QSTARZ_BLE_TREE_SCHEMA: Lazy<Value> = Lazy::new(|| {
             })
 });
 
-fn init_qstarz_trees() {
+fn setup_parse_trees() {
     if !is_tree_planted(QSTARZ_BLE_SCHEMA_TREE_ID) {
         plant_tree(QSTARZ_BLE_SCHEMA_TREE_ID, QSTARZ_BLE_TREE_SCHEMA.clone());
     }
@@ -116,7 +116,7 @@ fn init_qstarz_trees() {
 #[test]
 fn test_qstarz_ble_packet_gps_not_fixed()
 {
-    init_qstarz_trees();
+    setup_parse_trees();
     /*
     19:03:33.506 - Characteristic (6E400004-B5A3-F393-E0A9-E50E24DCCA9E) notified: <01547801 00000000 00000080 00000000 00000080> 	01=GPS is not fixed
     19:03:33.506 - Characteristic (6E400004-B5A3-F393-E0A9-E50E24DCCA9E) notified: <85f2de60 61a6fd3f 00000000 14aeca42 6800a9ff>
@@ -186,7 +186,7 @@ fn test_qstarz_ble_packet_gps_not_fixed()
 #[test]
 fn test_qstarz_ble_packet_gps_fixed_wo_gsv()
 {
-    init_qstarz_trees();
+    setup_parse_trees();
     /*
     19:03:34.403 - Characteristic (6E400004-B5A3-F393-E0A9-E50E24DCCA9E) notified: <0354c800 cd94d6df 8a91a340 821e6adb 2eadc740>  	03=GPS is fixed
     19:03:34.403 - Characteristic (6E400004-B5A3-F393-E0A9-E50E24DCCA9E) notified: <86f2de60 3a924340 10c89943 ec51c842 610077ff>
@@ -216,8 +216,8 @@ fn test_qstarz_ble_packet_gps_fixed_wo_gsv()
                 "g_sensor_z": 114.0/256.0,
                 "hdop": 1.7899999618530273,
                 "heading_degrees": 100.16000366210938,
-                "latitude": 25.04771239,
-                "longitude": 121.22366071,
+                "latitude": 25.079520650000003,
+                "longitude": 121.37276785,
                 "max_snr": 19,
                 "millisecond": 200,
                 "rcr": 84,
@@ -235,7 +235,7 @@ fn test_qstarz_ble_packet_gps_fixed_wo_gsv()
 #[test]
 fn test_qstarz_ble_packet_gps_fixed_w_gsv()
 {
-    init_qstarz_trees();
+    setup_parse_trees();
     /*
     19:03:34.555 - Characteristic (6E400004-B5A3-F393-E0A9-E50E24DCCA9E) notified: <03549001 faf202ec 8b91a340 69519fe4 2eadc740>  	03=GPS is fixed
     19:03:34.555 - Characteristic (6E400004-B5A3-F393-E0A9-E50E24DCCA9E) notified: <86f2de60 40de4b40 aec79943 5c8fd442 480082ff>
@@ -258,8 +258,8 @@ fn test_qstarz_ble_packet_gps_fixed_w_gsv()
                 "fix_status_matched": "3D",
                 "rcr": 84,
                 "millisecond": 400,
-                "latitude": 25.047732850000003,
-                "longitude": 121.22366351999999,
+                "latitude": 25.079554750000003,
+                "longitude": 121.37277253333332,
                 "timestamp_s": 1625223814,
                 "float_speed_kmh": 3.1854400634765625,
                 "float_height_m": 307.55999755859375,
@@ -300,6 +300,43 @@ fn test_qstarz_ble_packet_gps_fixed_w_gsv()
             }
             )
     );
+    //////////////////////////
+
+}
+
+
+#[test]
+fn test_qstarz_ble_packet_250113_002143_csv()
+{
+    setup_parse_trees();
+
+    /*
+INDEX	RCR	UTC DATE	UTC TIME	LOCAL DATE	LOCAL TIME	MS	VALID	LATITUDE	N/S	LONGITUDE	    E/W	HEIGHT (m)	SPEED (km/h)	HEADING	    G-sensor(X)	G-sensor(Y)	G-sensor(Z)	PDOP	HDOP	VDOP	NSAT	Max. SNR
+1	   T	2025/1/13	0:21:43	    2025/1/13	8:21:43	    900	3D-Fix	25.06878535	N	121.5914897333	E	69	        2.019	        79.222996	-0.0039	    0.0078	    0.9922	2.3	2.3	0.2	6	39
+    */
+    let data = hex_to_bin(r#"
+        03 54 84 03 45 F3 00 16 41 90 A3 40 98 89 22 A4 BE B3 C7 40 97 5C 84 67 0E 32 01 40 5E 7A 8B 42 8F 62 AB 43 FF FF 02 00 FE 00 27 00 8F C2 15 40 3D 0A 57 3E 0A 06 01 5A 00 00 00 00 00 00 00 00
+        "#
+    );
+    let parsed_json = parse_tree(QSTARZ_BLE_SCHEMA_TREE_ID, &data).unwrap();
+    println!("parsed_json: {}", serde_json::to_string_pretty(&parsed_json).unwrap());
+    assert_eq!("25.06878535", format!("{:.8}",parsed_json["latitude"].as_f64().unwrap()));
+    assert_eq!("121.5914897333", format!("{:.10}",parsed_json["longitude"].as_f64().unwrap()));
+    //TODO: assert all other fields in csv
+
+    /*
+INDEX	RCR	UTC DATE	UTC TIME	LOCAL DATE	LOCAL TIME	MS	VALID	LATITUDE	N/S	LONGITUDE	    E/W	HEIGHT (m)	SPEED (km/h)	HEADING	    G-sensor(X)	G-sensor(Y)	G-sensor(Z)	PDOP	HDOP	VDOP	NSAT	Max. SNR
+2	T	2025/1/13	0:21:44	2025/1/13	8:21:44	0	3D-Fix	25.0687856	N	121.5914911833	E	68	1.519	172.589507	-0.0039	0.0078	1.0039	2.5	2.3	1	6	39
+    */
+    let data = hex_to_bin(r#"
+        03 54 00 00 4A 44 F8 17 41 90 A3 40 AC 58 FC A6 BE B3 C7 40 98 5C 84 67 CC 62 C2 3F 62 50 88 42 33 D3 A8 43 FF FF 02 00 01 01 27 00 AE 47 11 40 00 00 80 3F 0A 06 01 5A 00 00 00 00 00 00 00 00
+        "#
+    );
+    let parsed_json = parse_tree(QSTARZ_BLE_SCHEMA_TREE_ID, &data).unwrap();
+    println!("parsed_json: {}", serde_json::to_string_pretty(&parsed_json).unwrap());
+    assert_eq!("25.0687856", format!("{:.7}",parsed_json["latitude"].as_f64().unwrap()));
+    assert_eq!("121.5914911833", format!("{:.10}",parsed_json["longitude"].as_f64().unwrap()));
+
     //////////////////////////
 
 }
