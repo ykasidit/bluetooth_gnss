66a14f3a69dd996990faced32ef0aaa88b19353f
Kasidit Yusuf <ykasidit@gmail.com>
2025-10-19 10:32:02 +0700

    draft

--- android/app/src/main/java/com/clearevo/libbluetooth_gnss_service/queue_to_outputstream_writer_thread.java
+++ android/app/src/main/java/com/clearevo/libbluetooth_gnss_service/queue_to_outputstream_writer_thread.java
@@ -11,7 +11,7 @@ public class queue_to_outputstream_writer_thread extends Thread implements Close
     ConcurrentLinkedQueue<byte[]> m_queue;
     OutputStream m_os;
     final String TAG = "btgnss_qtowt";
-    public static final int SLEEP_IF_NO_DATA_MILLIS = 200;
+    public static final int SLEEP_IF_NO_DATA_MILLIS = 1;
 
     public queue_to_outputstream_writer_thread(ConcurrentLinkedQueue<byte[]> queue, OutputStream os)
     {
@@ -27,22 +27,21 @@ public class queue_to_outputstream_writer_thread extends Thread implements Close
         } catch (Exception e) {
         }
         m_os = null;
-        this.interrupt();
         m_queue = null;
+        this.interrupt();
+
     }
 
     public void run()
     {
-        Log.d(TAG, "queue_to_outputstream_writer_thread thread start");
-        try {
+        Log.d(TAG, "queue_to_outputstream_writer_thread "+hashCode()+" start");
+        try (this) {
             while (true) {
                 //System.out.println("m_queue poll pre poll");
                 byte[] out_buf = m_queue.poll();
                 //Log.d(TAG,"queue_to_outputstream_writer_thread: m_queue poll buf:" + out_buf);
                 if (out_buf != null && out_buf.length > 0) {
                     m_os.write(out_buf);
-                } else {
-                    Thread.sleep(SLEEP_IF_NO_DATA_MILLIS);
                 }
             }
         } catch (Exception e) {
@@ -50,8 +49,8 @@ public class queue_to_outputstream_writer_thread extends Thread implements Close
                 Log.d(TAG, "queue_to_outputstream_writer_thread thread ending with exception: " + Log.getStackTraceString(e));
             }
         } finally {
-            close();
+            Log.d(TAG, "queue_to_outputstream_writer_thread "+hashCode()+" done");
         }
-        Log.d(TAG, "queue_to_outputstream_writer_thread thread ended");
+        Log.d(TAG, "inputstream_to_queue_reader_thread "+hashCode()+" end");
     }
 }
